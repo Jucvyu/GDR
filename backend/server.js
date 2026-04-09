46c039fca70d08ef079810f8d89331ff49600d88
Jucvyu <99603654+Jucvyu@users.noreply.github.com>
2026-04-09 18:55:14 -0500

    Update cors

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,10 @@ dotenv.config({
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "https://gdr-beige.vercel.app",
+    credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
